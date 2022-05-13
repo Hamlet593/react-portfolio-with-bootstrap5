503e80c125c1bb11e671097a41042eec53fa4a32
Hamlet593 <hamomuradyan93@gmail.com>
2022-05-13 16:36:31 +0400

    Change hamburger menu icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import myLogo from '../logo.png';
+// REACT FONTAWSEOME IMPORTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
@@ -13,7 +16,10 @@ const Navbar = () => {
                 />
             </a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <FontAwesomeIcon
+                    icon={faBars}
+                    style={{color: '#fff'}}
+                />
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto">
