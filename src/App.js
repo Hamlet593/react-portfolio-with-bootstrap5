650047d7b3eda396ed3a68686100c58f1553566e
Hamo Muradyan <hamomuradyan93@gmail.com>
2022-05-22 18:34:29 +0400

    convert to scss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Particles from "react-tsparticles";
+import './App.scss';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
-                enable: true,
-                value_area: 900,
-              }
-            },
-          },
-        }}
-      />
       <Navbar />
       <Header />
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div className="container">
             <a className="navbar-brand" href="#">
                 <img 
@@ -22,7 +22,7 @@ const Navbar = () => {
                 />
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto">
+                <ul className="navbar-nav ms-auto central_block">
                     <li className="nav-item">
                         <a className="nav-link active" href="#">Մեր մասին</a>
                     </li>
